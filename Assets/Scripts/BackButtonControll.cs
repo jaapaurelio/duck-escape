4499e8cce189ce5373b77914c779473ef2c83398
jaapaurelio <jaapaurelio@gmail.com>
2014-11-29 12:27:51 +0000

    Adicionado o botão voltar atras no ecrã de Game over. Alteração do tempo da animação no ecrã de game over.

---
+++ Assets/Scripts/BackButtonControll.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackButtonControll : MonoBehaviour {
+
+	void OnMouseDown () {
+		GameObject gameManager = GameObject.Find( "GameManager" );
+		
+		GameManagerControll gameManagerControll = gameManager.GetComponent<GameManagerControll>();
+		
+		gameManagerControll.StartMainMenuScene();
+
+	}
+}

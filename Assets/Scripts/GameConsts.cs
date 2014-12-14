3b0147036852a8a997927bb55f1833c0c6233a33
jaapaurelio <jaapaurelio@gmail.com>
2014-12-14 05:51:53 +0000

    Ajustes nas velocidades

--- Assets/Scripts/GameConsts.cs
+++ Assets/Scripts/GameConsts.cs
@@ -5,11 +5,11 @@ public class GameConsts {
 
 
 	public const float AimInitialSpeed = 1.2f;
-	public const float AimLevelSpeedIncreaser = 0.3f;
+	public const float AimLevelSpeedIncreaser = 0.2f;
 	public const float AimFollowingDuckIncreaser = 0.4f;
 
 	public const float DuckInitialSpeed = 1f;
-	public const float DuckLevelSpeedIncrease = 0.3f;
+	public const float DuckLevelSpeedIncrease = 0.2f;
 	public const float DuckSpeedIncrease = 0.5f;
 	
 	public static float LevelTime = 20.0f;	// secconds

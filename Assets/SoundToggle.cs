6aaa212bc712df34a4edeeb6996c8080e73cdd5d
jaapaurelio <jaapaurelio@gmail.com>
2015-01-02 02:29:28 +0000

    Removido log

--- Assets/SoundToggle.cs
+++ Assets/SoundToggle.cs
@@ -8,8 +8,8 @@ public class SoundToggle : Button {
 	
 	public override void OnButtonClick() {
 		allAudioSources = FindObjectsOfType(typeof(AudioSource)) as AudioSource[];
+
 		foreach( AudioSource audioS in allAudioSources) {
-			Debug.Log(audioS.mute);
 			audioS.mute = true;
 		}
 	}
